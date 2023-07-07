5a647953a1c753c28d60268b6868ed133dcb2c62
shaddydevops <122651230+shaddydevops@users.noreply.github.com>
2023-07-07 15:26:04 +0100

    completion of data ingestion stage

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from textsummurizer.pipeline.stage_o1_data_ingestion import DataIngestionTrainingPipeline
+
 from textsummurizer.logging  import logger 
 
-logger.info("welcome to our custom logger")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>>> stage{STAGE_NAME} started <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/textsummurizer/pipeline/stage_o1_data_ingestion.py
@@ -0,0 +1,16 @@
+from textsummurizer.config.configuration import ConfigurationManager
+from textsummurizer.components.data_ingestion import DataIngestion
+from textsummurizer.logging import logger
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
