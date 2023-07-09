584d3a7d9d3c91f7dbff46c8fd9564b9f1673069
shaddydevops <122651230+shaddydevops@users.noreply.github.com>
2023-07-10 00:13:13 +0100

    data validation stage completed
    Added data validation to the pipeline

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from textsummurizer.pipeline.stage_o1_data_ingestion import DataIngestionTrainingPipeline
-
+from textsummurizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textsummurizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textsummurizer.logging  import logger 
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
-    logger.info(f">>>>>>> stage{STAGE_NAME} started <<<<<")
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+   
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx============x")
 except Exception as e:
     logger.exception(e)
---
+++ src/textsummurizer/pipeline/stage_02_data_validation.py
@@ -0,0 +1,15 @@
+from textsummurizer.config.configuration import ConfigurationManager
+from textsummurizer.components.data_validation import DataValidation
+from textsummurizer.logging import logger
+
+
+class DataValidationTrainingPipeline  :
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_files_exist()
+
